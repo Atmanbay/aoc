84d979ed8b125fbd2e874c70601476c22a1ce38d
Andrew Hachten <ajhachten@gmail.com>
2024-12-02 08:20:14 -0500

    2024 Day 2

---
+++ 2024/src/days/2.py
@@ -0,0 +1,48 @@
+from itertools import pairwise
+
+
+def parse_input(input):
+    return [
+        list(map(int, line.split()))
+        for line in input
+    ]
+
+
+def is_safe(report):
+    differences = [b - a for a, b in pairwise(report)]
+
+    is_increasing = all(diff > 0 for diff in differences)
+    is_decreasing = all(diff < 0 for diff in differences)
+    within_range = all(1 <= abs(diff) <= 3 for diff in differences)
+
+    return (is_increasing or is_decreasing) and within_range
+
+
+def part1(input):
+    safe_count = 0
+    reports = parse_input(input)
+
+    for report in reports:
+        if is_safe(report):
+            safe_count += 1
+
+    return safe_count
+
+
+def can_be_safe(report):
+    for i in range(len(report)):
+        if is_safe(report[:i] + report[i+1:]):
+            return True
+        
+    return False
+
+
+def part2(input):
+    safe_count = 0
+    reports = parse_input(input)
+
+    for report in reports:
+        if is_safe(report) or can_be_safe(report):
+            safe_count += 1
+
+    return safe_count
